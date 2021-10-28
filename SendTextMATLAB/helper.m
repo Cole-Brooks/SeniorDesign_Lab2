53b4bb700bf2ee437ee905b8a7c11a6048554ac0
Hongyu Zeng <zdszym@me.com>
2021-10-27 19:00:00 -0500

    using MATLAB for RTC and text feature

---
+++ SendTextMATLAB/helper.m
@@ -0,0 +1,23 @@
+hasBeenAlerted = false;
+s = serialport('COM8', 115200);
+while true
+   res = str2double(readline(s)); 
+   if res == 1
+       hasBeenAlerted = false;
+   else
+       if ~hasBeenAlerted
+           c = fix(clock);
+           hour = c(4);
+           minue = c(5);
+           ampm = "AM";
+           if hour > 14
+               hour = mod(hour, 12);
+               ampm = "PM";
+           end
+
+           warning = sprintf("Critical safety event at %02d:%02d  %s", hour, minue, ampm);
+           send_text_message("319-383-9547", "att", "WARNING", warning);
+           hasBeenAlerted = true;
+       end
+   end
+end
